7a2950e05495b3aa02f31f7b4910a18e52cb575a
Ashish Maharjan <amaharjan1032@gmail.com>
2020-07-09 00:54:46 +0545

    Issue solved using hard reload

--- Ecom_dennis/settings.py
+++ Ecom_dennis/settings.py
@@ -120,9 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-MEDIA_URL = '/images/'
